983d30e5f4ee21314339a34f841f25e8216dd0a3
brad <brad>
2020-05-17 16:14:40 -0500

    Added

---
+++ coronavirus-cases/src/app/cases.ts
@@ -0,0 +1,12 @@
+export class Cases {
+  _id: string;
+  name: string;
+  gender: string;
+  age: number;
+  address: string;
+  city: string;
+  country: string;
+  status: string;
+  updated: Date;
+}
+
---
+++ coronavirus-cases/src/app/statistic.ts
@@ -0,0 +1,5 @@
+export class Statistic {
+  _id: any;
+  count: number;
+}
+
